f55e98d3ae5dd87dc90e6fe33dd7bbd4951fb1dd
Madeleyner Fernandez <129197262+Madeleyner@users.noreply.github.com>
2025-05-01 02:45:01 +0000

    todo-list-MF

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [showX, setShowX] = useState("");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handlePressKey = (e) => {
+    if (e.key === "Enter") {
+      setTodoList([...todoList, newTodo]);
+      setNewTodo("");
+    }
+  };
+
+  const handleDelete = (indexToDelet) => {
+    setTodoList(todoList.filter((elem, index) => index !== indexToDelet));
+  };
+
+  return (
+    <div className="box text-center">
+      <div className="contenedor-principal">
+        <h1 className="h1">Lista de tareas pendientes</h1>
+        <input
+          type="text"
+          className="holder"
+          placeholder="¿Qué tareas necesitas realizar?"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          onKeyDown={handlePressKey}
+        />
+        <ul className="todo-list">
+          {todoList.map((todo, index) => (
+            <div>
+              <li
+                key={index}
+                className="item"
+                onMouseOver={() => setShowX(index)}
+                onMouseLeave={() => setShowX(null)}
+              >
+                {todo}
+                {showX === index && (
+                  <small className="mx-5" onClick={() => handleDelete(index)}>
+                    🗑️
+                  </small>
+                )}
+              </li>
+            </div>
+          ))}
+          <li className="footer">
+            {todoList.length === 0
+              ? "No hay tareas, añadir tareas"
+              : todoList.length + " tareas por hacer"}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
